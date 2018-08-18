ad5b0ba90ea4fd43edf2dfd2158f73bcd47f0528
CaoMengjiao <mengjiao.cao@teradata.com>
2018-08-18 16:21:49 +0800

    shopping

--- src/main/java/com/shop/server/service/impl/WxServiceImpl.java
+++ src/main/java/com/shop/server/service/impl/WxServiceImpl.java
@@ -5,22 +5,35 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.shop.server.service.WxService;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class WxServiceImpl implements WxService {
     @Override
     public void setDataByText(Map<String, String> map) {
-        map.put("text", JSON.toJSONString(Map.of("content","Hello World"),SerializerFeature.UseSingleQuotes));
+//        map.put("text", JSON.toJSONString(Map.of("content","Hello World"),SerializerFeature.UseSingleQuotes));
+        Map<String,String> mapV = new HashMap<>();
+        mapV.put("content","Hello World");
+        map.put("text", JSON.toJSONString(mapV,SerializerFeature.UseSingleQuotes));
     }
 
     @Override
     public void setDataByImage(Map<String, String> map) {
-        map.put("image", JSON.toJSONString(Map.of("media_id","MEDIA_ID")));
+//        map.put("image", JSON.toJSONString(Map.of("media_id","MEDIA_ID")));
+        Map<String,String> mapV = new HashMap<>();
+        mapV.put("content","Hello World");
+        map.put("image", JSON.toJSONString(mapV));
     }
 
     @Override
     public void setDataByLink(Map<String, String> map) {
-        map.put("link", JSON.toJSONString(Map.of("title","Happy Day","description","Is Really A Happy Day","url","URL","thumb_url","THUMB_URL")));
+//        map.put("link", JSON.toJSONString(Map.of("title","Happy Day","description","Is Really A Happy Day","url","URL","thumb_url","THUMB_URL")));
+        Map<String,String> mapV = new HashMap<>();
+        mapV.put("title","Happy Day");
+        mapV.put("description","Is Really A Happy Day");
+        mapV.put("url","URL");
+        mapV.put("thumb_url","THUMB_URL");
+        map.put("link", JSON.toJSONString(mapV));
     }
 }
